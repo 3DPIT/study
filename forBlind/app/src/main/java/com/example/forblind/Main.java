5af6010d85720bd1eb9158802c73bf9e69927392
3DPIT <oksos0817@naver.com>
2019-11-25 19:59:22 +0900

    블루투스 데이터 전송받는 부분만 일부 수정하고 xml 수정 완료

--- forBlind/app/src/main/java/com/example/forblind/Main.java
+++ forBlind/app/src/main/java/com/example/forblind/Main.java
@@ -2,32 +2,20 @@ package com.example.forblind;
 
 import android.Manifest;
 import android.app.AlertDialog;
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.Vibrator;
-import android.telephony.SmsManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CompoundButton;
 import android.widget.ImageView;
-import android.widget.ProgressBar;
-import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -37,14 +25,10 @@ import androidx.core.app.ActivityCompat;
 
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.io.UnsupportedEncodingException;
-import java.net.URLDecoder;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 import java.util.UUID;
-
-import static java.lang.Thread.sleep;
 
 public class Main extends AppCompatActivity implements View.OnClickListener {
 
@@ -83,11 +67,7 @@ public class Main extends AppCompatActivity implements View.OnClickListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        ((Button) findViewById(R.id.btn_angle_main_menu)).setOnClickListener(this);
-        ((Button) findViewById(R.id.btn_main_display_start)).setOnClickListener(this);
-        ((Button) findViewById(R.id.btn_angle_main_bluetooth)).setOnClickListener(this);
-        tv_angle_number = (TextView) findViewById(R.id.tv_H_pos);
-        img_angle = (ImageView) findViewById(R.id.img_angle);
+        ((Button) findViewById(R.id.btn_main_bluetooth)).setOnClickListener(this);
 
         Intent r_angle_main_user = getIntent();
         angle_main_userName = r_angle_main_user.getStringExtra("이름");
@@ -98,19 +78,7 @@ public class Main extends AppCompatActivity implements View.OnClickListener {
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-           /* case R.id.btn_angle_main_menu:
-                Intent p_action_ska = new Intent(Angle_Main.this, Main.class);
-                p_action_ska.putExtra("이름", angle_main_userName);
-                p_action_ska.putExtra("카운터", count_sign);
-                startActivity(p_action_ska);
-                break;
-            case R.id.btn_main_display_start:
-                Intent p_action_ikqmw = new Intent(Angle_Main.this, Angle_Display.class);
-                p_action_ikqmw.putExtra("이름", angle_main_userName);
-                p_action_ikqmw.putExtra("카운터", count_sign);
-                startActivity(p_action_ikqmw);
-                break;*/
-            case R.id.btn_angle_main_bluetooth:
+            case R.id.btn_main_bluetooth:
                 checkBluetooth();
                 break;
         }
@@ -175,59 +143,17 @@ public class Main extends AppCompatActivity implements View.OnClickListener {
                                                     if (data == null || data.equals("") == true || data.equals(" ") == true || data.equals("  ") == true) {
                                                         // 처리
                                                     } else if (data.length() >= 10 && data.length() < 15 && data instanceof String) {
-
-
                                                         Log.d("Data값 :", data);
 
-
- /* New Position
-                                                        array = data.split("/");
-                                                        Log.d("전화 :", array[0]);
-                                                        Log.d("각도 :", array[1]);
-                                                        Log.d("카운터 :", array[2]);
-
-                                                        tv_angle_number.setText(array[1]);
-
-                                                        for (int i = 0; i < array.length; i++)
-                                                          System.out.format("array[%d] = %s%n", i, array[i]);
-                                                        }
-*/
-
-                                                        /* 허균이1 소스 시작 *//*
-data.length() >= 8 && data.length() < 10 &&
+                                                       // pass_value_1 = data;
+                                                       // pass_value_2 = data;
                                                         pass_value_1 = data;
-                                                        pass_value_2 = data;
-                                                        //pass_value_3 = data;
-                                                        count_sign = pass_value_1.substring(0, 3);
-                                                        angle_sign = pass_value_2.substring(3, 6);
-                                                        //call_sign = pass_value_3.substring(8, 11);
-                                                        tv_angle_number.setText(angle_sign);
-*//* 허균이1 소스 끝 */
 
+                                                        call_sign = pass_value_1.substring(0,3);
 
-                                                        /* 허균이2 소스 시작 *//**/
-                                                        pass_value_1 = data;
-                                                        pass_value_2 = data;
-                                                        pass_value_3 = data;
-                                                        count_sign = pass_value_1.substring(0, 3);
-                                                        angle_sign = pass_value_2.substring(4, 7);
-                                                        call_sign = pass_value_3.substring(8, 11);
-                                                        tv_angle_number.setText(angle_sign);
-
-//                                                        AIP = Integer.parseInt(angle_sign);
-//                                                        if (0 < AIP && AIP <= 75) {
-//                                                            img_angle.setImageDrawable(getResources().getDrawable(R.drawable.angle_70, getApplicationContext().getTheme()));
-//                                                        } else if (120 <= AIP && AIP <= 150) {
-//                                                            img_angle.setImageDrawable(getResources().getDrawable(R.drawable.angle_120, getApplicationContext().getTheme()));
-//                                                        } else {
-//                                                            img_angle.setImageDrawable(getResources().getDrawable(R.drawable.angle_90, getApplicationContext().getTheme()));
-//                                                        }
-
+//
 //                                                        CIP = Integer.parseInt(call_sign);
-//                                                        Log.d("count_sign", String.valueOf(count_sign));
-//                                                        Log.d("angle_sign", String.valueOf(angle_sign));
 //                                                        Log.d("call_sign", String.valueOf(call_sign));
-//                                                        Log.d("CIP", String.valueOf(CIP));
 
                                                         if (call_sign.equals("111")) {
                                                             Intent intentxx = new Intent(Intent.ACTION_CALL, Uri.parse("tel:01068887481"));
@@ -243,12 +169,7 @@ data.length() >= 8 && data.length() < 10 &&
                                                             }
                                                             startActivity(intentxx);
                                                         }
-
-
-
                                                     }
-
-
                                                 }
                                             }, 1500);
                                         }
@@ -261,15 +182,13 @@ data.length() >= 8 && data.length() < 10 &&
                     } catch (Exception e) {
                         Toast.makeText(getApplicationContext(), "데이터 수신 중 오류가 발생 했습니다.", Toast.LENGTH_LONG).show();
                         Log.d("예외처리 값", e.toString());
-                        finish();
+                       // finish();
                     }
                 }
             }
         });
-
         mWorkerThread.start();
     }
-
     void selectDevice() {
         mDevices = mBluetoothAdapter.getBondedDevices();
         mPariedDeviceCount = mDevices.size();
@@ -278,9 +197,8 @@ data.length() >= 8 && data.length() < 10 &&
             Toast.makeText(getApplicationContext(), "페어링된 장치가 없습니다.", Toast.LENGTH_LONG).show();
             finish();
         }
-
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("블루투스 장치 선택");
+        builder.setTitle("제품에 맞는 블루투스를 잡아주세요.");
 
         List<String> listItems = new ArrayList<String>();
 
@@ -288,19 +206,18 @@ data.length() >= 8 && data.length() < 10 &&
             listItems.add(device.getName());
         }
 
-        listItems.add("취소");
+        listItems.add("Cancel");
 
         final CharSequence[] items = listItems.toArray(new CharSequence[listItems.size()]);
         listItems.toArray(new CharSequence[listItems.size()]);
 
         builder.setItems(items, new DialogInterface.OnClickListener() {
-
             @Override
             public void onClick(DialogInterface dialog, int item) {
                 // TODO Auto-generated method stub
                 if (item == mPariedDeviceCount) {
                     Toast.makeText(getApplicationContext(), "연결할 장치를 선택하지 않았습니다.", Toast.LENGTH_LONG).show();
-                    finish();
+                   // finish();
                 } else {
                     connectToSelectedDevice(items[item].toString());
                 }
@@ -325,7 +242,6 @@ data.length() >= 8 && data.length() < 10 &&
                 selectDevice();
         }
     }
-
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         switch (requestCode) {
@@ -334,7 +250,7 @@ data.length() >= 8 && data.length() < 10 &&
                     selectDevice();
                 } else if (resultCode == RESULT_CANCELED) {
                     Toast.makeText(getApplicationContext(), "블루투스를 사용할 수 없어 프로그램을 종료합니다", Toast.LENGTH_LONG).show();
-                    finish();
+                    //finish();
                 }
                 break;
         }
