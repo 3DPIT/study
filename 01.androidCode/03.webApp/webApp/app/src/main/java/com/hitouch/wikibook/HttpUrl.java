05aece940afaef8133b49fec2e5da721fba3827a
3DPIT <47946136+3DPIT@users.noreply.github.com>
2021-10-30 23:08:18 +0900

    [21.10.30] docs: 웹앱 개발 진행

---
+++ 01.androidCode/03.webApp/webApp/app/src/main/java/com/hitouch/wikibook/HttpUrl.java
@@ -0,0 +1,6 @@
+package com.hitouch.wikibook;
+
+public class HttpUrl {
+    public static final String WEBURL = "https://3dpit.tistory.com/";
+	//public static final String WEBURL = "http://www.naver.com";								 
+}
