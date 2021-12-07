000de3c7790ec8b0f4d0b72a328cd60b2e164b4f
km.park <kyeongmin.park@mirero.co.kr>
2021-12-07 10:09:29 +0900

    [21.12.07] docs: Csv사용법 문서 작성

---
+++ 02.studyData/05.C#/2021/12/1207/CsvHelper/code/csvHelperTest/csvHelperTest/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using CsvHelper;
+using CsvHelper.Configuration;
+using CsvHelper.TypeConversion;
+using csvHelperTest;
+
+namespace csvHelperTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string _path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"/csv/file.csv";
+            using (var reader = new StreamReader(_path))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                csv.Context.RegisterClassMap<FooMap>();
+                var records = csv.GetRecords<Foo>();
+            }
+        }
+    }
+    public enum ClassInfo
+    {
+        oneGrade,
+        twoGrade,
+        threeGrade
+    }
+    public class Foo
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ClassInfo Class { get; set; }
+    }
+    public sealed class FooMap : ClassMap<Foo>
+    {
+        public FooMap()
+        {
+            Map(m => m.Id);
+            Map(m => m.Name);
+            Map(m => m.Class).TypeConverter<ClassInfoConverter<ClassInfo>>();
+        }
+    }
+    public class ClassInfoConverter<T> : DefaultTypeConverter
+    {
+        public override object ConvertFromString(string text, IReaderRow row, MemberMapData memberMapData)
+        {
+           // return ClassInfoConverter.DeserializeObject<T>(text);
+           return Enum.Parse<ClassInfo>(text);
+        }
+
+        public override string ConvertToString(object value, IWriterRow row, MemberMapData memberMapData)
+        {
+            //return ClassInfoConverter.SerializeObject(value);
+            return value.ToString();
+        }
+    }
+}
