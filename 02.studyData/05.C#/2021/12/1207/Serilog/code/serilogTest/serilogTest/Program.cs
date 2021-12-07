c27369db514eb38c526c7059f08dc1dbc00a28ee
km.park <kyeongmin.park@mirero.co.kr>
2021-12-07 09:09:58 +0900

    [21.12.07] docs: Serilog 사용법 문서 작성

---
+++ 02.studyData/05.C#/2021/12/1207/Serilog/code/serilogTest/serilogTest/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Serilog;
+using Serilog.Core;
+using Serilog.Events;
+
+
+namespace serilogTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var log = new LoggerConfiguration()
+                .MinimumLevel.Information()
+                .WriteTo.Console(restrictedToMinimumLevel:LogEventLevel.Error)
+                .WriteTo.File("log.txt")
+                .CreateLogger();
+            
+            log.Debug("Debug");
+            log.Information("Information");
+            log.Warning("Warning");
+            log.Error("Error");
+            log.Fatal("Fatal");
+
+            Log.CloseAndFlush();
+        }
+    }
+}
