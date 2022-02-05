a0bc34826ae9edf5230a93893937e034bb75fd79
3dpit <oksos0817@naver.com>
2022-02-05 20:01:22 +0900

    [22.02.05] docs: 프로퍼티 목차 생성

---
+++ 02.studyData/05.Csharp/2022/02/typroheaderFind/typora/typora/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace typora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string local = Environment.GetFolderPath(Environment.SpecialFolder.Desktop); ;
+            string path = local + @"\typora\test.md";
+            string textValue = System.IO.File.ReadAllText(path);
+            Console.WriteLine(textValue);
+            List<string> l = new List<string>();
+
+            StringBuilder s = new StringBuilder();
+            int flag = 0;
+            foreach (var word in textValue)
+            {
+                if (word == '#')
+                {
+
+                    flag = 1;
+                    continue;
+                }
+
+                if (flag == 1 && word != '\r' && word != '\n')
+                {
+                    s.Append(word);
+                }
+
+                if (flag == 1 && word == '\r')
+                {
+                    s.Append('\n');
+                    flag = 0;
+
+                }
+            }
+            Console.WriteLine(l);
+            string spath = local + @"\typora\text.md";
+            System.IO.File.WriteAllText(spath, s.ToString(), Encoding.Default);
+        }
+    }
+}
