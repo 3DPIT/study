13720c32048b1179cdc2fcac136c620a3dc40fa9
3dpit <oksos0817@naver.com>
2022-02-21 19:54:21 +0900

    [22.02.21] docs: postgres, grafana

---
+++ 02.studyData/06Docker/0.GrafanaAndPostgres/src/grafana-postgres/grafana/dashboard/datasource.js
@@ -0,0 +1,15 @@
+{
+        "name": "postgres-datasource",
+        "type": "postgres",
+        "typeLogoUrl": "public/app/plugins/datasource/postgres/img/postgresql_logo.svg",
+        "access": "proxy",
+        "url": "172.19.0.2:5432",
+        "password": "postgres",
+        "user": "postgres",
+        "database": "postgres",
+        "basicAuth": false,
+        "isDefault": true,
+        "jsonData": {
+            "sslmode": "disable"
+        }
+}
