95bdda234690eb03b26055f1822b91b0ed2af19a
kyeongmin.park <oksos0817@naver.com>
2022-03-16 14:15:33 +0900

    [22.03.16] 애트리뷰트

---
+++ 02.studyData/05.Csharp/2022/애트리뷰트/src/BasicAttribute/BasicAttribute/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace BasicAttribute;
+
+class MyClass
+{
+    [Obsolete("OldMethod는 폐기되었습니다. NewMethod()를 이용하세요")]
+    public void OldMethod()
+    {
+        Console.WriteLine("OLD");
+    }
+
+    public void NewMethod()
+    {
+        Console.WriteLine("NEW");
+    }
+}
+
+class MainApp
+{
+    static void Main(string[] args)
+    {
+        MyClass myClass = new MyClass();
+        myClass.OldMethod();
+        myClass.NewMethod();
+    }
+}
---
+++ 02.studyData/05.Csharp/2022/애트리뷰트/src/HistoryAttribute/HistoryAttribute/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace HistoryAttribute;
+
+[System.AttributeUsage(System.AttributeTargets.Class, AllowMultiple = true)]
+class History : System.Attribute
+{
+    private string programmer;
+    public double version;
+    public string changes;
+
+    public History(string programmer)
+    {
+        this.programmer = programmer;
+        version = 1.0;
+        changes = "First release";
+    }
+
+    public string GetProgrammer()
+    {
+        return programmer;
+    }
+
+}
+
+[History("Sean", version = 0.1, changes = "2022-03-15 Created class stub")]
+[History("Bob", version = 0.2, changes = "2022-03-16 Added Func() Method")]
+
+class MyClass
+{
+    public void Func()
+    {
+        Console.WriteLine("Func()");
+    }
+}
+class MainApp
+{
+    static void Main(string[] args)
+    {
+        Type type = typeof(MyClass);
+        Attribute[] attributes = Attribute.GetCustomAttributes(type);
+
+        Console.WriteLine("MyClass history ...");
+
+        foreach(Attribute a in attributes)
+        {
+            History h = a as History;
+            if(h!=null)
+            {
+                Console.WriteLine("Ver:{0}, Programmer:{1}, Changes:{2}",h.version,h.GetProgrammer(),h.changes);
+            }
+        }
+    }
+}
