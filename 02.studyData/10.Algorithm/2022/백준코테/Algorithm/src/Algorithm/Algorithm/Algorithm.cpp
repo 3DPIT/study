eb0f7c4e5cfe88ff922619f44de9219d368d3e1e
3dpit <oksos0817@naver.com>
2022-04-10 19:08:24 +0900

    [22.04.10] docs: 14502 연구소 풀이 완료 및 알고리즘 산출

--- 02.studyData/10.Algorithm/2022/백준코테/Algorithm/src/Algorithm/Algorithm/Algorithm.cpp
+++ 02.studyData/10.Algorithm/2022/백준코테/Algorithm/src/Algorithm/Algorithm/Algorithm.cpp
@@ -1,1 +1,65 @@
-﻿
+﻿#include<iostream>
+#include<stdio.h>
+#include<vector>
+#include<string.h>
+#define NS 100
+using namespace std;
+
+int main(void)
+{
+	int N;
+	int board[NS][NS];
+	scanf("%d", &N);
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			scanf("%d", &board[i][j]);
+		}
+	}
+
+	vector<int>q;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			if (board[i][j] != 0) {
+				q.push_back(board[i][j]);
+				board[i][j] = 0;
+			}
+		}
+
+		for (int j = 0; j < q.size()-1; j++) {
+			if (q.size() == 0)break;
+			if (q[j] != 0 && q[j] == q[j + 1])
+			{
+				q[j] += q[j + 1];
+				q.erase(q.begin() + j + 1);
+			}
+		}
+		for (int j = 0;q.size()!=0; j++) {
+			board[i][j] = q.front();
+			q.erase(q.begin());
+		}
+	}
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			printf("%d", board[i][j]);
+		}
+		printf("\n");
+	}
+	return 0;
+}
+/*
+5
+2 0 2 0 2
+2 2 2 2 2
+2 2 0 2 2
+2 0 4 0 2
+4 0 2 2 0
+*/
+
+/*
+4 2 0 0 0
+4 4 2 0 0
+4 4 0 0 0
+2 4 2 0 0
+4 4 0 0 0
+*/
