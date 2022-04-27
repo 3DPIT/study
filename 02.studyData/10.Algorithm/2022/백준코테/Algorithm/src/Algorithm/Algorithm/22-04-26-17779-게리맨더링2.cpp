b54d421dead38b5dd44bc242d1450e5d67c3f527
3dpit <oksos0817@naver.com>
2022-04-27 20:46:30 +0900

    [22.04.27] 17837 새로운게임2 업로드

--- 02.studyData/10.Algorithm/2022/백준코테/Algorithm/src/Algorithm/Algorithm/22-04-26-17779-게리맨더링2.cpp
+++ 02.studyData/10.Algorithm/2022/백준코테/Algorithm/src/Algorithm/Algorithm/22-04-26-17779-게리맨더링2.cpp
@@ -18,7 +18,7 @@ bool safeZone(int y, int x) {
 }
 void init() {
 	retMax = 0x80000000;
-	retMin = ret=0x7fffffff;
+	retMin = ret = 0x7fffffff;
 	scanf("%d", &N);
 	for (int i = 1; i <= N; i++) {
 		for (int j = 1; j <= N; j++) {
@@ -30,44 +30,38 @@ void five(int x, int y, int d1, int d2) {
 	memset(visit, 0, sizeof(visit));
 	vector<Data>v1;
 	vector<Data>v2;
-	for (int i = x, j = y; i < x + d1, j >y - d1; i++,j--) {
-		if(safeZone(i, j))
+	for (int i = x, j = y; i < x + d1, j >y - d1; i++, j--) {
+		if (safeZone(i, j))
 			v1.push_back({ i,j });
 	}
 	for (int i = x, j = y; i < x + d2, j < y + d2; i++, j++) {
 		if (safeZone(i, j))
 
-		v2.push_back({ i,j });
+			v2.push_back({ i,j });
 
 	}
 	for (int i = x + d1, j = y - d1; i <= x + d1 + d2, j <= y - d1 + d2; i++, j++) {
 		if (safeZone(i, j))
 
-		v1.push_back({ i,j });
+			v1.push_back({ i,j });
 	}
 	for (int i = x + d2, j = y + d2; i <= x + d2 + d1, j >= y + d2 - d1; i++, j--) {
 		if (safeZone(i, j))
 
-		v2.push_back({ i,j });
+			v2.push_back({ i,j });
 	}
 
-	
-	//for (int i = 0; i < v1.size(); i++) {
-	//		for (int xx = v1[i].x; xx <= v2[i].x; xx++) {
-	//			for (int yy = v1[i].y; yy <= v2[i].y; yy++) {
 
-	//			if(safeZone(xx,yy))
-	//			visit[xx][yy] = 5;
-	//		}
-	//	}
-	//}
 	for (int i = 0; i < v1.size(); i++) {
-		int x = v1[i].x;
-		for (int y = v1[i].y; y <= v2[i].y; y++) {
-			visit[x][y] = 5;
+		for (int xx = v1[i].x; xx <= v2[i].x; xx++) {
+			for (int yy = v1[i].y; yy <= v2[i].y; yy++) {
+
+				if (safeZone(xx, yy))
+					visit[xx][yy] = 5;
+			}
 		}
 	}
-	int a;
+
 	//for (int i = 0; i < v1.size(); i++) {
 	//	for (int y = v1[i].y; y <= v2[i].y; y++) {
 	//		for (int x = v1[i].x; x <= v2[i].x; x++) {
@@ -95,8 +89,8 @@ void five(int x, int y, int d1, int d2) {
 	}
 	printf("\n");*/
 }
-void one(int x,int y,int d1, int d2) {
-	for(int i = 1; i < x + d1; i++) {
+void one(int x, int y, int d1, int d2) {
+	for (int i = 1; i < x + d1; i++) {
 		for (int j = 1; j <= y; j++) {
 			if (visit[i][j] == 0) {
 				visit[i][j] = 1;
@@ -106,7 +100,7 @@ void one(int x,int y,int d1, int d2) {
 }
 void two(int x, int y, int d1, int d2) {
 	for (int i = 1; i <= x + d2; i++) {
-		for (int j = y+1; j <= N; j++) {
+		for (int j = y + 1; j <= N; j++) {
 			if (visit[i][j] == 0) {
 				visit[i][j] = 2;
 			}
@@ -124,8 +118,8 @@ void three(int x, int y, int d1, int d2) {
 }
 void four(int x, int y, int d1, int d2) {
 
-	for (int i = x + d2+1; i <= N; i++) {
-		for (int j = y - d1+d2; j <= N; j++) {
+	for (int i = x + d2 + 1; i <= N; i++) {
+		for (int j = y - d1 + d2; j <= N; j++) {
 			if (visit[i][j] == 0) {
 				visit[i][j] = 4;
 			}
@@ -157,7 +151,7 @@ void elect() {
 							retMax = retMax < number[n] ? number[n] : retMax;
 							retMin = retMin > number[n] ? number[n] : retMin;
 						}
-						int retnum =retMax - retMin;
+						int retnum = retMax - retMin;
 						ret = ret > retnum ? retnum : ret;
 					}
 				}
---
+++ 02.studyData/10.Algorithm/2022/백준코테/Algorithm/src/Algorithm/Algorithm/22-04-26-17837-새로운게임2.cpp
@@ -0,0 +1,148 @@
+#include<stdio.h>
+#include<iostream>
+#include<vector>
+#define NS 14
+using namespace std;
+int N, K, ret;
+int flag;
+int dy[] = { 0,0,0,-1,1 };
+int dx[] = { 0,1,-1,0,0 };
+bool safeZone(int y, int x) {
+	return 1 <= y && y <= N && 1 <= x && x <= N;
+}
+struct Data {
+	int y, x, dir, num;
+};
+int initBoard[NS][NS];//색깔이 입력된 배열
+vector<int>board[NS][NS];//말이 입력된 배열
+Data horse[NS];//말의 위치가 저장된 배열
+
+int white(int number, int y, int x, int dir) {
+	Data n;
+	n.num = number;
+
+	n.y = y + dy[dir];
+	n.x = x + dx[dir];
+	int chk = 0;
+	for (int i = 0; i < board[y][x].size(); i++) {
+		if (board[y][x][i] == number || chk) {
+			chk = 1;
+			board[n.y][n.x].push_back(board[y][x][i]);//변경
+			horse[board[y][x][i]].y = n.y;
+			horse[board[y][x][i]].x = n.x;
+			board[y][x].erase(board[y][x].begin() + i); //삭제
+			i--;
+		}
+	}
+	if (board[n.y][n.x].size() >= 4)return 1;
+	return 0;
+}
+int red(int number, int y, int x, int dir) {
+	Data n;
+	n.num = number;
+	n.y = y + dy[dir];
+	n.x = x + dx[dir];
+	n.dir = dir;
+	int chk = 0;
+	vector<int>num;
+	for (int i = 0; i < board[y][x].size(); i++) {
+		if (board[y][x][i] == number || chk) {
+			chk = 1;
+			num.push_back(board[y][x][i]);//번호저장
+			horse[board[y][x][i]].y = n.y;
+			horse[board[y][x][i]].x = n.x;
+			board[y][x].erase(board[y][x].begin() + i); //삭제
+			i--;
+		}
+	}
+	for (int i = num.size() - 1; i >= 0; i--) {
+		board[n.y][n.x].push_back(num[i]);//변경
+	}
+	if (board[n.y][n.x].size() >= 4)return 1;
+	return 0;
+}
+int blue(int number, int y, int x, int dir) {
+	Data n;
+	n.num = number;
+	if (1 == dir)dir = 2;
+	else if (2 == dir)dir = 1;
+	else if (3 == dir)dir = 4;
+	else if (4 == dir) dir = 3;
+	n.y = y + dy[dir];
+	n.x = x + dx[dir];
+	horse[number].dir = dir;//방향 변경
+	if (!safeZone(n.y, n.x) || initBoard[n.y][n.x] == 2) {
+		return 0;
+	}
+	else if (initBoard[n.y][n.x] == 1) {
+		red(number, y, x, dir);
+	}
+	else if (initBoard[n.y][n.x] == 0) {
+		white(number, y, x, dir);
+	}
+	return 0;
+}
+
+int chk() {
+
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= N; j++) {
+			if (board[i][j].size() >= 4) {
+				return 1;
+			}
+		}
+	}
+	return 0;
+}
+void newGame() {
+	for (int k = 1; k <= K; k++) {//말 정보 입력
+		Data c;
+		scanf("%d %d %d", &c.y, &c.x, &c.dir);
+		horse[k] = c;
+		board[c.y][c.x].push_back(k);
+	}
+	while (ret != 1001) {
+		for (int k = 1; k <= K; k++) {
+			Data c = horse[k];
+			Data n = c;
+			n.y = c.y + dy[c.dir]; n.x = c.x + dx[c.dir];
+			if (!safeZone(n.y, n.x) || initBoard[n.y][n.x] == 2) {
+				if (blue(k, horse[k].y, horse[k].x, horse[k].dir)) {
+					flag = 1;
+					break;
+				}
+			}
+			else if (initBoard[n.y][n.x] == 0) {
+				if(white(k,horse[k].y,horse[k].x,horse[k].dir)) {
+					flag = 1;
+					break;
+				}
+			}
+			else if (initBoard[n.y][n.x] == 1) {
+				if(red(k, horse[k].y, horse[k].x, horse[k].dir)) {
+					flag = 1;
+					break;
+				}
+			}
+			if (chk()) break;
+		}
+		if (chk()) break;
+		ret++;
+	}
+}
+
+void init() {
+	scanf("%d %d", &N, &K);
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= N; j++) {
+			scanf("%d", &initBoard[i][j]);
+		}
+	}
+}
+int main(void)
+{
+	init();
+	newGame();
+	printf("%d\n", ret == 1001 ? -1 : ret+1);
+	return 0;
+}
