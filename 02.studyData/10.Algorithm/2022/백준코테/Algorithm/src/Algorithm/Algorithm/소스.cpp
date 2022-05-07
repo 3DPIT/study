2b7987f55aa855adccc2eb74da37bb216a668aeb
3dpit <oksos0817@naver.com>
2022-05-07 22:27:08 +0900

    no message

--- 02.studyData/10.Algorithm/2022/백준코테/Algorithm/src/Algorithm/Algorithm/소스.cpp
+++ 02.studyData/10.Algorithm/2022/백준코테/Algorithm/src/Algorithm/Algorithm/소스.cpp
@@ -1,96 +1,213 @@
-#include<iostream>
 #include<stdio.h>
+#include<iostream>
 #include<vector>
+#include<algorithm>
+#define NS 100
 using namespace std;
-#define NS 54
+int N, M, H, K;
+int dy[] = { 0,0,1,-1 };
+int dx[] = { 1,-1,0,0};
+
+int dyN[] = { -1,0,1,0 };
+int dxN[] = { 0,1,0,-1 };
+
+int dy0[] = { 1,0,-1,0 };
+int dx0[] = { 0,1,0,-1 };
+
 struct Data {
-	int y, x, m, s, d;
+	int y, x, dir,num;
 };
-int N, M, K, ret;
-int dy[] = { -1,-1,0,1,1,1,0,-1 };
-int dx[] = { 0,1,1,1,0,-1,-1,-1,-1 };
-vector<Data>fireBall;
+vector<Data>run;
+vector<Data>runBoard[NS][NS];
+int board[NS][NS];
+int sBoard[NS][NS];//술래 위치
+Data S;//술래
+int ret;
+int rotationNum[205];
+
+bool safeZone(int y, int x) {
+	return 0 <= y && y < N && 0 <= x && x < N;
+}
 
 void init() {
-	scanf("%d %d %d", &N, &M, &K);
-	for (int i = 0; i < M; i++) {
-		Data c;
-		scanf("%d %d %d %d %d", &c.y, &c.x, &c.m, &c.s, &c.d);
-		c.y--;
-		c.x--;
-		c.s = (c.s + N) % N;
-		fireBall.push_back(c);
+	ret = 0;
+	run.clear();
+	memset(rotationNum, 0, sizeof(rotationNum));
+	memset(board, 0, sizeof(board));
+	memset(sBoard, 0, sizeof(sBoard));
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			runBoard[i][j].clear();
+		}
 	}
+
+	scanf("%d %d %d %d", &N, &M, &H, &K);
+
+	int idx = 0;
+	for (int n = 1; n < N; n++) {
+		rotationNum[idx++] = n;
+		rotationNum[idx++] = n;
+	}
+	rotationNum[idx++] = N-1;
+
+	for (int m = 1; m <= M; m++) {
+		int y, x, dir;
+		scanf("%d %d %d", &y, &x, &dir);
+		run.push_back({ y-1,x-1,dir,m });
+		runBoard[y][x].push_back({ y-1,x-1,dir,m });
+	}
+
+	for (int h = 1; h <= H; h++) {
+		int y, x;
+		scanf("%d %d", &y, &x);
+		board[y-1][x-1] = -1;
+	}
+	S.y = (N /2); S.x = (N / 2);
+	sBoard[S.y][S.x] = 1;
 }
-void fireBallMove() {
-	while (K--) {
-		for (int i = 0; i < fireBall.size(); i++) {//파이어볼 이동
-			Data c = fireBall[i];
-			Data n = c;
-			for (int s = 0; s < c.s; s++) {
-				n.y = c.y + dy[c.d];
-				n.x = c.x + dx[c.d];
-				c.y = n.y%N;
-				c.x = n.x;
-			}
-			fireBall[i] = n;
-		}
-		vector<int>fireBalls[NS][NS];
-		for (int f = 0; f < fireBall.size(); f++) {
-			Data c = fireBall[f];
-			fireBalls[c.y][c.x].push_back(f);
-		}
 
-		vector<Data>savefireBalls;
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				if (fireBalls[i][j].size() == 0)continue;
-				int index = fireBalls[i][j][0];
-				if (fireBalls[i][j].size() == 1) savefireBalls.push_back(fireBall[index]);
-				if (fireBalls[i][j].size() >= 2) {
-					int sum_m = 0, sum_s = 0;
-					bool odd = true,
-						even = true;
-					for (int f = 0; f < fireBalls[i][j].size(); f++) {
-						sum_m += fireBall[fireBalls[i][j][f]].m;
-						sum_s += fireBall[fireBalls[i][j][f]].s;
-						if (fireBall[fireBalls[i][j][f]].d % 2 == 0) {
-							odd = false;
-						}
-						else {
-							even = false;
-						}
-					}
-					sum_m = sum_m / 5;
-					sum_s = sum_s / fireBalls[i][j].size();
-					if (sum_m == 0) continue;
-					if (even || odd) {//0 2 4 6 
-						for (int dir = 0; dir <= 6; dir += 2) {
-							savefireBalls.push_back({ i,j,sum_m,sum_s,dir });
-						}
-					}
-					else {// 1 3 5 7
-						for (int dir = 1; dir <= 7; dir += 2) {
-							savefireBalls.push_back({ i,j,sum_m,sum_s,dir });
-						}
-					}
+void play() {
+	int flag = 0;
+	int idx = 0;
+	int gidx = 0;
+	int ggidx = 0;
+	int nidx = rotationNum[0];
+	for (int k1 = 1; k1 <= K; k1++) {
+		for (int k = 0; k < M; k++) {
+			if (run[k].dir == -1) continue;
+			int D = abs(run[k].y - S.y) + abs(run[k].x - S.x);
+			if (D <= 3) {
+				Data n;
+				n.dir = run[k].dir;
+				n.y = run[k].y + dy[n.dir];
+				n.x = run[k].x + dx[n.dir];
+				if (sBoard[n.y][n.x] == -1) continue;
+				if (safeZone(n.y, n.x) && sBoard[n.y][n.x] != -1) {//범위에 있고 술레위치가 아니면
+					for (int s = 0; s < runBoard[run[k].y][run[k].x].size(); s++) {
+						if (runBoard[run[k].y][run[k].x][s].num == k + 1) {
+							runBoard[run[k].y][run[k].x].erase(runBoard[run[k].y][run[k].x].begin() + s);
 
+							runBoard[n.y][n.x].push_back({ n.y,n.x,n.dir,k });//이동완료
+							run[k] = n;
+
+						}
+
+					}
 				}
-			}//for j
-		}//for i
+				else if (!safeZone(n.y, n.x)) {
+					if (n.dir == 1)n.dir = 0;
+					else if (n.dir == 0)n.dir = 1;
+					else if (n.dir == 2) n.dir = 3;
+					else if (n.dir == 3) n.dir = 2;
+					n.y = S.y + dy[n.dir];
+					n.x = S.x + dx[n.dir];
+					if (sBoard[n.y][n.x] == -1) continue;
+					if (safeZone(n.y, n.x) && sBoard[n.y][n.x] != -1) {//범위에 있고 술레위치가 아니면
+						for (int s = 0; s < runBoard[S.y][S.x].size(); s++) {
+							if (runBoard[run[k].y][run[k].x][s].num == k) {
+								runBoard[run[k].y][run[k].x].erase(runBoard[run[k].y][run[k].x].begin() +s);
 
-		fireBall.clear();
-		for (int f = 0; f < savefireBalls.size(); f++) {
-			fireBall.push_back(savefireBalls[f]);
+								runBoard[n.y][n.x].push_back({ n.y,n.x,n.dir,k });//이동완료
+								run[k] = n;
+
+							}
+
+						}
+					}
+				}
+			}// D<=3
+		}
+
+		//술래 이동
+		if(flag == 0) {
+
+			sBoard[S.y][S.x] = 0;
+			S.y = S.y + dyN[idx];
+			S.x = S.x + dxN[idx];
+			sBoard[S.y][S.x] = -1;
+			gidx++;
+
+			if (gidx == nidx) {
+				idx++;
+				if (idx == 4) idx = 0;
+				gidx = 0;
+				nidx = rotationNum[ggidx++];
+			}
+
+			for (int dir = 0; dir < 3; dir++) {
+				int ny = S.y + dy[idx] * dir;
+				int nx = S.x + dx[idx] * dir;
+				if (board[ny][nx] != -1) {
+					if (runBoard[ny][nx].size() == 0) continue;
+					ret += runBoard[ny][nx].size()*k1;
+					for (int s = 0; s < runBoard[S.y][S.x].size(); s++) {
+						if (runBoard[S.y][S.x][s].num == runBoard[ny][nx][s].num) {
+							int num = runBoard[ny][nx][s].num;
+							runBoard[S.y][S.x].erase(runBoard[S.y][S.x].begin() + s);
+
+							runBoard[ny][nx].push_back({ ny,nx,idx,num });//이동완료
+							run[k1].y = -1;
+							run[k1].x = -1;
+							run[k1].dir = -1;
+							break;
+						}
+
+					}
+				}
+			}
+
+			if (S.y == 0 && S.x == 0) {
+				flag = 1;
+				idx = 0;
+			}
+		}
+		else if (flag == 1) {
+
+			sBoard[S.y][S.x] = 0;
+			S.y = S.y + dy0[idx];
+			S.x = S.x + dx0[idx];
+			sBoard[S.y][S.x] = -1;
+
+
+			if (gidx == nidx) {
+				idx++;
+				gidx = 0;
+				nidx = rotationNum[gidx];
+			}
+
+			for (int dir = 0; dir < 3; dir++) {
+				int ny = S.y + dy[idx] * dir;
+				int nx = S.x + dx[idx] * dir;
+				if (board[ny][nx] == -1) {
+					ret += runBoard[ny][nx].size()*k1;
+					for (int s = 0; s < runBoard[S.y][S.x].size(); s++) {
+						if (runBoard[S.y][S.x][s].num == runBoard[ny][nx][s].num) {
+							int num = runBoard[ny][nx][s].num;
+							runBoard[S.y][S.x].erase(runBoard[S.y][S.x].begin() + s);
+
+							runBoard[ny][nx].push_back({ ny,nx,idx,num });//이동완료
+							run[k1].y = -1;
+							run[k1].x = -1;
+							run[k1].dir = -1;
+							break;
+						}
+
+					}
+				}
+			}
+
+			if (S.y == N-1/2 && S.x == N-1/2) {
+				flag = 0;
+				idx = 0;
+			}
 		}
 	}
-
-	for (int f = 0; f < fireBall.size(); f++) {
-		ret += fireBall[f].m;
-	}
 }
+
 int main(void) {
 	init();
-	fireBallMove();
-	printf("%d\n", ret);
+	play();
+	cout << ret;
+	return 0;
 }
