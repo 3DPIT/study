eea0cfc2ee3716dabf1170af831d073733b076c3
3dpit <oksos0817@naver.com>
2022-06-26 16:38:32 +0900

    [22.06.26] code: 021-카카오인턴-수식최대화 코드 업로드

---
+++ 02.studyData/10.Algorithm/2022/백준코테/Algorithm/src/Algorithm/Algorithm/2020-카카오인턴십-수식최대화.cpp
@@ -0,0 +1,81 @@
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+string ordering[6][3] = {
+	//{"+-","*","."},
+	//{"+*","-","."},
+	//{"-*","+","."},
+	//
+	//{"*","+-","."},
+	//{"-","+*","."},
+	//{"+","-*","."},
+
+	{"+","-","*"},
+	{"+","*","-"},
+	{"-","+","*"},
+	{"-","*","+"},
+	{"*","-","+"},
+	{"*","+","-"}
+};
+long long solution(string expression) {
+	long long answer = 0;
+	vector<string>calNumber;
+	string number;
+	for (int i = 0; i < expression.size(); i++) {
+		if ('0' <= expression[i] && expression[i] <= '9') {
+			number += expression[i];
+		}
+		else {
+			calNumber.push_back(number);
+			number.clear();
+			number += expression[i];
+			calNumber.push_back(number);
+			number.clear();
+		}
+		if(i== expression.size()-1)
+			calNumber.push_back(number);
+	}
+
+
+	vector<string>copy_calNumber=calNumber;
+
+	for (int i = 0; i < 6; i++) {
+		for (int j = 0; j < 3; j++) {
+			long long sum = 0;
+			for (int e = 0; e < calNumber.size(); e++) {
+				if (calNumber[e] == ordering[i][j]) {
+					int firstNum = stol (calNumber[e - 1]);
+					int secondNum = stol(calNumber[e + 1]);
+
+					if (calNumber[e] == "+") {
+						sum = firstNum + secondNum;
+					}
+					else if (calNumber[e] == "-") {
+						sum = firstNum - secondNum;
+					}
+					else if (calNumber[e] == "*") {
+						sum = firstNum * secondNum;
+					}
+					else break;
+					calNumber.erase(calNumber.begin() + (e - 1), calNumber.begin() + (e - 1) + 3);
+					e--;
+					string number = to_string(sum);
+					calNumber.insert(calNumber.begin() + e, number);
+				}
+			}
+		} 
+		answer = max(answer, abs(stoll(calNumber[0])));
+		calNumber = copy_calNumber;
+
+	}
+	return answer;
+}
+
+int main(void)
+{
+	cout<<solution("50*6-3*2");
+	return 0;
+}
