34ee6b74053bd250a82f4b0761201729a55c2438
3dpit <oksos0817@naver.com>
2022-06-29 23:40:29 +0900

    [22.06.29] code add: 카카오 문제 풀이 순위검색 문제 못품

---
+++ 02.studyData/10.Algorithm/2022/백준코테/Algorithm/src/Algorithm/Algorithm/22-06-29-2021KAKAO순위검색.cpp
@@ -0,0 +1,48 @@
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+#include <sstream>
+
+		using namespace std;
+
+		const string ALL = "-";
+		unordered_map<string, vector<int>> map;
+
+		void insert(string* key, int mask, int point) {
+			string s = "";
+			for (int i = 0; i < 4; i++) {
+				s += (mask & (1 << i)) ? ALL : key[i];
+				map[s].push_back(point);
+			}
+		}
+
+		vector<int> solution(vector<string> info, vector<string> query) {
+			vector<int> answer;
+			string key[4], tmp;
+			int point;
+
+			for (auto& inf : info) {
+				istringstream iss(inf);
+				iss >> key[0] >> key[1] >> key[2] >> key[3] >> point;
+				for (int i = 0; i < 16; i++) insert(key, i, point);
+			}
+
+			for (auto& m : map) sort(m.second.begin(), m.second.end());
+
+			for (auto& que : query) {
+				istringstream iss(que);
+				iss >> key[0] >> tmp >> key[1] >> tmp >> key[2] >> tmp >> key[3] >>
+					point;
+				string s = key[0] + key[1] + key[2] + key[3];
+				vector<int>& v = map[s];
+				answer.push_back(v.end() - lower_bound(v.begin(), v.end(), point));
+			}
+
+			return answer;
+		}
+int main(void)
+{
+	vector<int> a = solution({ "java backend junior pizza 150", "python frontend senior chicken 210", "python frontend senior chicken 150", "cpp backend senior pizza 260", "java backend junior chicken 80", "python backend senior chicken 50" },
+		{ "java and backend and junior and pizza 100","python and frontend and senior and chicken 200","cpp and - and senior and pizza 250","- and backend and senior and - 150","- and - and - and chicken 100","- and - and - and - 150" });
+	return 0;
+}
