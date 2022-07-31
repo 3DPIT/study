7ab354954bf8668cb1ca95c1463b36de95cc7b6a
3dpit <oksos0817@naver.com>
2022-07-31 23:58:19 +0900

    [22.07.31] code: 방금그곡 소스 업로드

--- 02.studyData/10.Algorithm/2022/백준코테/Algorithm/src/Algorithm/Algorithm/22-07-28-2018카카오-[3차]방금그곡.cpp
+++ 02.studyData/10.Algorithm/2022/백준코테/Algorithm/src/Algorithm/Algorithm/22-07-28-2018카카오-[3차]방금그곡.cpp
@@ -1,83 +1,79 @@
 #include <string>
 #include <vector>
 #include <unordered_map>
-#include <regex>
 using namespace std;
+
+void replaceAll(string &m, string from, string to) {
+	int pos = 0;
+	while ((pos = m.find(from)) != -1) {
+		m.replace(pos, from.length(), to);
+	}
+}
 
 struct Data {
 	int time;
 	string title;
-	string music;
 };
 string solution(string m, vector<string> musicinfos) {
 	string answer = "";
-	string a;
-	int idx = 0;
-	m = regex_replace(m, regex("C#"), "c");
-	m = regex_replace(m, regex("D#"), "d");
-	m = regex_replace(m, regex("F#"), "f");
-	m = regex_replace(m, regex("G#"), "g");
-	m = regex_replace(m, regex("A#"), "a");
-	vector<string> v[104];
-	vector<Data> musicV;
+	pair<int, string> ans = { 0,"" };
+	replaceAll(m, "A#", "a"); // 1. m에서 반올림된 음계를 모두 치환
+	replaceAll(m, "B#", "b");
+	replaceAll(m, "C#", "c");
+	replaceAll(m, "D#", "d");
+	replaceAll(m, "E#", "e");
+	replaceAll(m, "F#", "f");
+	replaceAll(m, "G#", "g");
+	
 	for (int i = 0; i < musicinfos.size(); i++) {
-		for (int j = 0; j < musicinfos[i].size(); j++) {
-			a += musicinfos[i][j];
-			if (musicinfos[i][j] == ',') {
-				v[i].push_back(a);
-				a.clear();
-			}
+
+		int t = 0,idx = 0, pos = 0;
+		string h1, h2, m1, m2, title, runningMusic;
+		h1 = musicinfos[i].substr(0, 2);
+		m1 = musicinfos[i].substr(3, 2);
+		h2 = musicinfos[i].substr(6, 2);
+		m2 = musicinfos[i].substr(9, 2);
+
+		t = abs(stoi(h1) - stoi(h2))*60;
+		if (m1 > m2) {
+			t -= 60;
+			t += (stoi(m2) - stoi(m1)) + 60;
 		}
-		v[i].push_back(a);
-		a.clear();
-	}
-	for (int i = 0; i < musicinfos.size(); i++) {
-		string time2;
-		string time1;
-		int time = 0;
-		int timeInt2 = 0;
-		int timeInt1 = 0;
-		timeInt2 = (v[i][1][0]-48)*10 + (v[i][1][1]-48);
-		time1 = v[i][0][0] + v[i][0][1];
-		timeInt1 = (v[i][0][0] - 48) * 10 + (v[i][0][1] - 48);
-		time += timeInt2 - timeInt1;
+		else t += stoi(m2) - stoi(m1);
+		idx = 12;
+		for (int j = 12; musicinfos[i][j] != ','; j++,idx++) {
+			title += musicinfos[i][j];
+		}
+		for (int j = idx+1; j < musicinfos[i].size(); j++) runningMusic += musicinfos[i][j];
+		replaceAll(runningMusic, "B#", "b");
+		replaceAll(runningMusic, "C#", "c");
+		replaceAll(runningMusic, "D#", "d");
+		replaceAll(runningMusic, "E#", "e");
+		replaceAll(runningMusic, "F#", "f");
+		replaceAll(runningMusic, "G#", "g");
 
-		timeInt2 = (v[i][1][3] - 48) * 10 + (v[i][1][4] - 48);
-		time1 = v[i][0][0] + v[i][0][1];
-		timeInt1 = (v[i][0][3] - 48) * 10 + (v[i][0][4] - 48);
-		time += timeInt2 - timeInt1;
-
-
-		int remainTime = time;
-		int idx = 0;
-		int size = v[i][3].size();
+		int remainTime = t;
+		idx = 0;
+		int size = runningMusic.size();
 		string music;
 		while (remainTime--) {
-			music += v[i][3][idx++];
+			music += runningMusic[idx++];
 			if (idx == size)idx = 0;
 		}
-		music = regex_replace(music, regex("C#"), "c");
-		music = regex_replace(music, regex("D#"), "d");
-		music = regex_replace(music, regex("F#"), "f");
-		music = regex_replace(music, regex("G#"), "g");
-		music = regex_replace(music, regex("A#"), "a");
-
-		
-		musicV.push_back({ time,v[i][2],music });
-	}
-	int max = 0x80000000;
-	string title;
-	for (int i = 0; i < musicV.size(); i++ ) {
-		if (musicV[i].music.find(m) != string::npos) {
-			if (max < musicV[i].time) {
-				max = musicV[i].time;
-				title = musicV[i].title;
-			}
+		idx = 0;
+		idx = music.find(m);
+		if (idx != -1 && ans.first < t) {
+			ans.first = t;
+			ans.second = title;
 		}
+
 	}
-	title.pop_back();
-	return title;
+	answer = ans.second;
+	if (answer == "") answer = "(None)";
+
+	return answer;
 }
 int main(void) {
 	solution("ABC#DEC#G", { "12:00,12:14,HELLO,CDEFGAB", "13:00,13:05,WORLD,ABCDEF" });
 }
+
