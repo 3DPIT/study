1ee1429c13b9b56062d49f0ea8810e0612a13a07
3dpit <oksos0817@naver.com>
2022-08-27 17:53:15 +0900

    [22.08.27] study code : 클래스 생성과 생성자 생성

---
+++ 02.studyData/05.Csharp/2022/0827_CS_StudyCode/CS_Study_220927/CS_Study_220927/Cat.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Study_220927
+{
+    public class Cat
+    {
+        public string Name;
+        public string Color;
+
+        public void Meow()
+        {
+            Console.WriteLine("{0} : 야옹", Name);
+        }
+    }
+}
