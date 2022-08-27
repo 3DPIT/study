d2300a9bb35c40af80015da32426d5aac988dde3
3dpit <oksos0817@naver.com>
2022-08-27 18:53:09 +0900

    [22.08.27] code: 얇은 복사에 대한 소스코드

--- 02.studyData/05.Csharp/2022/0827_CS_StudyCode/CS_Study_220927/CS_Study_220927/Program.cs
+++ 02.studyData/05.Csharp/2022/0827_CS_StudyCode/CS_Study_220927/CS_Study_220927/Program.cs
@@ -2,7 +2,19 @@
 
 namespace main;
 class Program
-{
+{ 
+    class MyClass
+    {
+        public int a;
+        public int b;
+    }
+
+    class MyClass2
+    {
+        public static int a;
+        public static int b;   
+    }
+
     public static void Main(string[] args)
     {
         Cat kitty = new Cat();
@@ -16,5 +28,22 @@ class Program
         nero.Name = "네로";
         nero.Meow();
         Console.WriteLine("{0} : {1}", kitty.Name, kitty.Color);
+
+        MyClass obj1 = new MyClass();
+        obj1.a = 1; 
+        obj1.b = 2;
+
+        MyClass2.a = 1;
+        MyClass2.b = 2;
+
+        MyClass source = new MyClass();
+        source.a = 10; 
+        source.b = 20;
+
+        MyClass target = source;
+        source.b = 30;
+
+        Console.WriteLine("{0} {1}", source.a, source.b);
+        Console.WriteLine("{0} {1}", target.a, target.b);
     }
 }
