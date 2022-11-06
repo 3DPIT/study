66a0f414dfea3a22e07a4d539b6ef43c67686449
3dpit <oksos0817@naver.com>
2022-11-06 22:18:33 +0900

    [22.11.06] docs: web update

--- 02.studyData/10.Algorithm/2022/11/1105/AlgorithmStudy/AlgorithmStudy/AlgorithmStudy.cpp
+++ 02.studyData/10.Algorithm/2022/11/1105/AlgorithmStudy/AlgorithmStudy/AlgorithmStudy.cpp
@@ -1,53 +1,32 @@
-﻿// AlgorithmStudy.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
-
+﻿#include <string>
 #include <iostream>
-#include <vector>
-#include <stdio.h>
+#include <math.h>
 using namespace std;
-void rotation(vector<vector<int>> &arr) {
-	vector<vector<int>>crr = arr;
 
-	for (int i = 0; i < arr.size(); i++) {
-		for (int j = 0; j < arr.size(); j++) {
-			crr[i][j] = arr[j][arr.size() - i - 1];
-		}
+string numberC;
+char numberChar[] = {
+	'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'
+};
+
+bool checkPrimeNumber(long long number) {
+	if (number == 0 || number == 1) return false;
+	for (int i = 2; i <= number*number; i++) {
+		if (number % i == 0) return false;
 	}
-
-	arr = crr;
+	return true;
 }
 
-int main()
-{
-	vector<vector<int>> a;
-	for (int i = 0; i < 3; i++) {
-		vector<int>a1;
-		for (int j = 0; j < 3; j++) {
-			int c;
-			cin >> c;
-			a1.push_back(c);
-		}
-		a.push_back(a1);
+bool checkPrimeNumber_test(long long number) {
+	if (number == 0 || number == 1) return false;
+	for (int i = 2; i <= sqrt(number); i++) {
+		if (number % i == 0)return false;
 	}
-	rotation(a);
-	for (int i = 0; i < 3; i++) {
-		for (int j = 0; j < 3; j++)
-		{
-			cout << a[i][j];
-		}
-			cout << endl;
-	}
-
-
+	return true;
 }
 
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
 
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
+int main(void) {
+
+	cout << checkPrimeNumber(4);
+	return 0;
+}
