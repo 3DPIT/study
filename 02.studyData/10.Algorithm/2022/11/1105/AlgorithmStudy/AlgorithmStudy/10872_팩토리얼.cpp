d81581d4c98ef9fe7ddfd61eefaf7abc7d26c36e
3dpit <oksos0817@naver.com>
2023-07-08 08:17:51 +0900

    feat : 수학 관련 알고리즘 완료

---
+++ 02.studyData/10.Algorithm/2022/11/1105/AlgorithmStudy/AlgorithmStudy/10872_팩토리얼.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<iostream>
+using namespace std;
+int factorial(int number) {
+	if (number == 0) return 1;
+
+	return number*factorial(number - 1);
+}
+
+
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int n;
+	cin >> n;
+	cout << factorial(n);
+
+	return 0;
+}
