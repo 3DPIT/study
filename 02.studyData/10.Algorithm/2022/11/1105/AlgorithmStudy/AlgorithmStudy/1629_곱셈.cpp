7fe39b17f8869422296ecedb2eeddc40777127a0
3dpit <oksos0817@naver.com>
2023-07-11 23:59:14 +0900

    code : 제곱근 정의 및 곱셈 문제 풀이

---
+++ 02.studyData/10.Algorithm/2022/11/1105/AlgorithmStudy/AlgorithmStudy/1629_곱셈.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+
+long long calc(long long a, long long b, long long c) {
+	if (b == 0) return 1;
+	else if (b == 1)return a;
+	else if (b % 2 == 0) {
+		long long temp = calc(a, b / 2,c);
+		return temp * temp %c;
+	}
+	else if (b % 2 == 1) {
+		return a * calc(a, b - 1,c) %c;
+	}
+}
+
+long long calc2(long long a, long long b, long long c) {
+	long long  ans = 1LL;
+	while (b > 0) {
+		if (b % 2 == 1) {
+			ans *= a;
+			ans %= c;
+		}
+		a = a * a ;
+		a %= c;
+		b /= 2;
+	}
+	return ans;
+}
+
+int main(void) {
+	
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	long long A, B, C;
+	cin >> A >> B >> C;
+	int result = calc2(A, B,C);
+	cout << result % C << '\n';
+	return 0;
+}
