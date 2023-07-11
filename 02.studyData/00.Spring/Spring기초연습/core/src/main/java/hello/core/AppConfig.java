6f4bcb7556be3c7c27795dd55de7d6766d4b0c09
Black <oksos0817@naver.com>
2023-07-11 23:13:33 +0900

    feat : singleton 보장해주는 spring bean

--- 02.studyData/00.Spring/Spring기초연습/core/src/main/java/hello/core/AppConfig.java
+++ 02.studyData/00.Spring/Spring기초연습/core/src/main/java/hello/core/AppConfig.java
@@ -16,19 +16,23 @@ public class AppConfig {
 
     @Bean
     public MemberService memberService(){
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
     public  MemoryMemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
     @Bean
     public OrderService orderService(){
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
     @Bean
     public DiscountPolicy discountPolicy(){
+        System.out.println("call AppConfig.discountPolicy");
         return new RateDiscouontPolicy();
     }
 }
