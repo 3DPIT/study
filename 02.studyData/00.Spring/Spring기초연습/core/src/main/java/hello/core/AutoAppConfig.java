da1d40528f7e32d312ecb056e8927c5d0f8a0c88
Black <oksos0817@naver.com>
2023-07-14 07:43:52 +0900

    feat : 어노테이션 이해, bean 등록 관련 테스트

---
+++ 02.studyData/00.Spring/Spring기초연습/core/src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,15 @@
+package hello.core;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(
+        basePackages = "hello.core",
+        basePackageClasses = AutoAppConfig.class,
+        excludeFilters = @ComponentScan.Filter(type= FilterType.ANNOTATION, classes=Configuration.class)
+)
+public class AutoAppConfig {
+
+}
