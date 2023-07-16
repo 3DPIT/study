8a7752d27971407111b2ad13f77c29279c7fa2d2
Black <oksos0817@naver.com>
2023-07-16 12:09:51 +0900

    feat : Qulifier 사용

--- 02.studyData/00.Spring/Spring기초연습/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ 02.studyData/00.Spring/Spring기초연습/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -5,6 +5,7 @@ import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -16,7 +17,7 @@ public class OrderServiceImpl implements OrderService{
     private final DiscountPolicy discountPolicy;
 
     @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+    public OrderServiceImpl(MemberRepository memberRepository, @Qualifier("mainDiscountPolicy") DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
