48ceddd9b35b5d6cea307275446b115612907c2c
Black <oksos0817@naver.com>
2023-07-16 12:18:50 +0900

    feat : annotation 만들고 사용하기

--- 02.studyData/00.Spring/Spring기초연습/core/src/main/java/hello/core/annotation/MainDiscountPolicy.java
+++ 02.studyData/00.Spring/Spring기초연습/core/src/main/java/hello/core/annotation/MainDiscountPolicy.java
@@ -1,2 +1,13 @@
-package hello.core.annotation;public class MainDiscountPolicy {
+package hello.core.annotation;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.CONSTRUCTOR, ElementType.METHOD, ElementType.PARAMETER, ElementType.FIELD, ElementType.ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Qualifier("mainDiscountPolicy")
+public @interface MainDiscountPolicy {
+
 }
