1f016211800a45d0087312d0843c7a495f3be490
Black <oksos0817@naver.com>
2023-08-16 00:46:27 +0900

    feat : jwt 까지 연결 완료

--- 02.studyData/11.Web/Backend/Spring/jot-tutorial/src/main/java/com/threedipt/tutorial/config/SecurityConfig.java
+++ 02.studyData/11.Web/Backend/Spring/jot-tutorial/src/main/java/com/threedipt/tutorial/config/SecurityConfig.java
@@ -1,31 +1,84 @@
 package com.threedipt.tutorial.config;
 
+import com.threedipt.tutorial.jwt.JwtAccessDeniedHandler;
+import com.threedipt.tutorial.jwt.JwtAuthenticationEntryPoint;
+import com.threedipt.tutorial.jwt.JwtSecurityConfig;
+import com.threedipt.tutorial.jwt.TokenProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @EnableWebSecurity
 @Configuration
-public class SecurityConfig extends WebSecurityConfiguration {
+@EnableGlobalMethodSecurity(prePostEnabled = true) // @PreAuthorize 어노테이션을 메소드단위로 추가하기 위해서 사용
+public class SecurityConfig {
+    @Autowired
+    private  TokenProvider tokenProvider;
+    @Autowired
+    private  JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    @Autowired
+    private  JwtAccessDeniedHandler jwtAccessDeniedHandler;
+
     @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfiguration) throws Exception {
-        AuthenticationManager authenticationManager = authConfiguration.getAuthenticationManager();
-        return authenticationManager;
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
+
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .anyRequest().authenticated();
+        http .csrf().disable()
+                .exceptionHandling()
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                .accessDeniedHandler(jwtAccessDeniedHandler)
 
+                .and()
+                .headers()
+                .frameOptions()
+                .sameOrigin()
 
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .apply(new JwtSecurityConfig(tokenProvider))
+
+                .and()
+                .authorizeHttpRequests().requestMatchers(
+                        new AntPathRequestMatcher("/**")).permitAll()
+                .and()
+                .csrf().ignoringRequestMatchers(
+                        new AntPathRequestMatcher("/h2-console/**"))
+                // 화면 깨짐 문제 해결 로직
+                .and()
+                .headers()
+                .addHeaderWriter(new XFrameOptionsHeaderWriter(
+                        // 헤더값 변경ß
+                        XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN))
+        ;
         return http.build();
+    }
+
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring()
+                .antMatchers(
+                        "h2-console/**"
+                        ,"/favicon.ico");
     }
 }
