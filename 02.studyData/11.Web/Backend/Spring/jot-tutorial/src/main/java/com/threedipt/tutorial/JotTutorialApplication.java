a3914bea1fb71ad581135bcc29deb817ee06e98c
Black <oksos0817@naver.com>
2023-08-16 00:15:32 +0900

    feat : h2 연동 에러 되는거 까지 완료, 이전 security에서 바뀐 방식으로 변경해서 적용

---
+++ 02.studyData/11.Web/Backend/Spring/jot-tutorial/src/main/java/com/threedipt/tutorial/JotTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.threedipt.tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JotTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JotTutorialApplication.class, args);
+	}
+
+}
