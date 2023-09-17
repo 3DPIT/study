d52acb09b22bdbc02e1e9638f965f161505efff5
Black <oksos0817@naver.com>
2023-09-18 01:40:36 +0900

    refactor : spring으로 적용하기

--- 02.studyData/11.Web/Backend/Spring/core/src/main/java/hello/core/AppConfig.java
+++ 02.studyData/11.Web/Backend/Spring/core/src/main/java/hello/core/AppConfig.java
@@ -1,4 +1,4 @@
-package hello.core.order;
+package hello.core;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
@@ -7,20 +7,27 @@ import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
     }
-
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
-
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
-    private DiscountPolicy discountPolicy() {
+    @Bean
+    public DiscountPolicy discountPolicy() {
         return new RateDiscountPolicy();
     }
 }
