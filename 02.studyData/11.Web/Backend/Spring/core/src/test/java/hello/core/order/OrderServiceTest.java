07fb71bdc469326fb6ff0bdfa0edbfc1d13d7458
Black <oksos0817@naver.com>
2023-09-18 01:04:11 +0900

    feat : appConfig 적용 1

--- 02.studyData/11.Web/Backend/Spring/core/src/test/java/hello/core/order/OrderServiceTest.java
+++ 02.studyData/11.Web/Backend/Spring/core/src/test/java/hello/core/order/OrderServiceTest.java
@@ -5,13 +5,19 @@ import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class OrderServiceTest {
 
-    MemberService memberService = new MemberServiceImpl();
-    OrderService orderService = new OrderServiceImpl();
-
+    MemberService memberService;
+    OrderService orderService;
+    @BeforeEach
+    public void beforeEash(){
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+        orderService = appConfig.orderService();
+    }
     @Test
     void createOrder(){
         Long memberId = 1L;
