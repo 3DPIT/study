412ee111bb8766a93aa67fa5fae30ec11cb77977
Black <oksos0817@naver.com>
2023-09-18 23:40:50 +0900

    feat : spring bean 조회

--- 02.studyData/11.Web/Backend/Spring/core/src/test/java/hello/core/beanfind/ApplicationContextInfoTest.java
+++ 02.studyData/11.Web/Backend/Spring/core/src/test/java/hello/core/beanfind/ApplicationContextInfoTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-public class ApplicationContextInfoTest {
+class ApplicationContextInfoTest {
     AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
 
     @Test
@@ -26,8 +26,9 @@ public class ApplicationContextInfoTest {
         for(String beanDefinitionName : beanDefinitionNames){
             BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
 
+            //ROLE_INFRASTRUCTURE : 스프링이 내부에서 사용하는 빈
+            //ROLE_APPLICATION: 직접 등록한 애플리케이션 빈
             if(beanDefinition.getRole()==BeanDefinition.ROLE_APPLICATION){
-//            if(beanDefinition.getRole()==BeanDefinition.ROLE_INFRASTRUCTURE){ //이외의 경우
                 Object bean = ac.getBean(beanDefinitionName);
                 System.out.println("name = "+ beanDefinitionName + "object = " + bean );
             }
