a9e1fa5aa0efb400d577098f8cd001ad81452a58
3dpit <oksos0817@naver.com>
2023-09-26 23:32:49 +0900

    code : 알고스팟 문제 풀이 완료

---
+++ 02.studyData/10.Algorithm/2022/11/1105/AlgorithmStudy/AlgorithmStudy/1261_알고스팟.cpp
@@ -0,0 +1,84 @@
+#include<stdio.h>
+#include<iostream>
+#include<queue>
+using namespace std;
+int N, M;
+
+struct Data {
+	int y; int x; int cnt;
+};
+int board[104][104];
+int dy[] = { 0,1,0,-1 };
+int dx[] = { 1,0,-1,0 };
+int visit[104][104];
+
+bool safeZone(Data n) {
+	return 0 <= n.y && n.y < M && 0 <= n.x && n.x < N;
+}
+
+int main(void) {
+
+	scanf("%d %d ", &N, &M);
+	for (int i = 0; i < M; i++) {
+		for (int j = 0; j < N; j++) {
+			scanf("%1d", &board[i][j]);
+			visit[i][j] = -1;
+		}
+	}
+
+	queue<Data>q;
+	queue<Data>nq;
+	q.push({ 0, 0 });
+	visit[0][0] = 0; 
+	int ret = 0x7fffffff;
+
+	while (!q.empty()) {
+		Data c = q.front(); q.pop();
+		if (c.y ==M-1 && c.x == N-1) {
+			ret = min(ret, c.cnt);
+		}
+
+		for (int dir = 0; dir < 4; dir++) {
+			Data n = c;
+			n.y = c.y + dy[dir];
+			n.x = c.x + dx[dir];
+
+			//if (safeZone(n)) {
+			//	if (visit[n.y][n.x] == -1) {
+			//		if (board[n.y][n.x] == 0) {
+			//			visit[n.y][n.x] = visit[c.y][c.x];
+			//			q.push(n);
+			//		}
+			//		else {
+			//			n.cnt++;
+			//			visit[n.y][n.x] = visit[c.y][c.x]+1;
+			//			nq.push(n);
+			//		}
+			//	}
+			//}
+
+			if (safeZone(n)&&visit[n.y][n.x] == -1&&board[n.y][n.x]==0) {
+				visit[n.y][n.x] = visit[c.y][c.x];
+				q.push(n);
+			}
+			else if (safeZone(n)&&visit[n.y][n.x] == -1 && board[n.y][n.x] == 1) {
+				n.cnt++;
+				visit[n.y][n.x] = visit[c.y][c.x]+1;
+				nq.push(n);
+			}
+		}
+		if (q.empty()) {
+			q = nq;
+			nq = queue<Data>();
+		}
+	}
+
+	if (ret == -1) {
+		cout << 0 << endl;
+	}
+	cout << visit[M- 1][N - 1] << endl;
+	//cout <<ret << endl;
+
+
+	return 0;
+}
