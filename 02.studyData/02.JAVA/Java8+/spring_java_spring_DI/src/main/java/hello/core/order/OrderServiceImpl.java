d8f35db06113b4a1fa00dd3d5b6fe6ae00511acd
3dpit <oksos0817@naver.com>
2023-11-02 07:27:13 +0900

    DI 주입전 상황까지 완료

--- 02.studyData/02.JAVA/Java8+/spring_java_spring_DI/src/main/java/hello/core/order/OrderServiceImpl.java
+++ 02.studyData/02.JAVA/Java8+/spring_java_spring_DI/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -2,13 +2,24 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixedDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService{
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixedDiscountPolicy();
+
+//    //private final DiscountPolicy discountPolicy = new FixedDiscountPolicy();
+//    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+//    // 역할 구현 충실하게 분리
+//    //현재 상태는 DIP 위반
+//    //현재 문제는 변경되는 순간 orderServiceImpl에서 코드 변경이 또 필요하다.
+//    //ocp위반, DIP 위반한 상황
+
+    //해결 코드
+    private DiscountPolicy discountPolicy; // 이렇게만 하면 null exception 생김 그래서 누군가가 따로 주입해줘야함
+
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = memberRepository.findById(memberId);
