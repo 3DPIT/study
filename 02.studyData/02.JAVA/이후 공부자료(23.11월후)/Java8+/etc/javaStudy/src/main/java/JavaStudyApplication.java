e00e5e4ed7f07d22ca8be59242f2cc0755a04792
Black <oksos0817@naver.com>
2023-12-03 15:30:22 +0900

    feat : commit msg test

--- 02.studyData/02.JAVA/이후 공부자료(23.11월후)/Java8+/etc/javaStudy/src/main/java/JavaStudyApplication.java
+++ 02.studyData/02.JAVA/이후 공부자료(23.11월후)/Java8+/etc/javaStudy/src/main/java/JavaStudyApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JavaStudyApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(JavaStudyApplication.class, args);
+        SpringApplication.run(JavaStudyApplication.class, args); 
     }
 
 }
