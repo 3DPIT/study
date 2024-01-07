6f7bf8698fb8927eee192da28f2e80a5422460dd
Black <oksos0817@naver.com>
2024-01-08 08:02:38 +0900

    feat : 진법변환 알고리즘 풀이
    - 익숙해질것
      - 두개 변수 연달아서 정수형인 경우 어떤식으로 뽑는것이 합리적일지 고민해보기

---
+++ 02.studyData/10.Algorithm/2023/java/algorithm/src/main/java/com/example/algorithm/알고리즘/완전탐색/_11005_진법변환2.java
@@ -0,0 +1,30 @@
+package com.example.algorithm.알고리즘.완전탐색;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class _11005_진법변환2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int number = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
+        List<Character> s = new ArrayList<>();
+        while(number >0){
+            int ret1 = number/N;
+            int ret2 = number%N;
+            if(ret1>=10) s.add((char) (ret2-10+'A'));
+            number = number /=N;
+        }
+
+        for(int i=s.size()-1;i>=0;i--){
+            bw.write(s.get(i));
+        }
+
+
+        bw.flush();
+    }
+}
