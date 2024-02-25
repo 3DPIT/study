65b8c5c5515277a4d6242ca8a11edc417fe4b3b3
Black <oksos0817@naver.com>
2024-02-26 07:43:07 +0900

    feat : 빈도정렬 업로드

--- 02.studyData/10.Algorithm/2023/java/algorithm/src/main/java/com/example/algorithm/알고리즘/정렬/_2910_빈도정렬.java
+++ 02.studyData/10.Algorithm/2023/java/algorithm/src/main/java/com/example/algorithm/알고리즘/정렬/_2910_빈도정렬.java
@@ -1,12 +1,35 @@
 package com.example.algorithm.알고리즘.정렬;
 
 
+import java.io.*;
+import java.util.*;
+
 /*
 https://www.acmicpc.net/problem/2910
  */
 public class _2910_빈도정렬 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int numberCount = Integer.parseInt(st.nextToken());
+
+        Set<Integer> inputSet = new HashSet<>();
+
+        Map<Integer , Integer> m = new HashMap<>();
+
+        String[] inputs = br.readLine().split(" ");
+        List<Integer> inputList = new ArrayList<>(inputs.length);
+
+        for(int i=0;i<N;i++){
+            int input = Integer.parseInt(inputs[i]);
+            inputSet.add(input);
+            inputㅣㅣㅣㄴaa
+        put(input,m.getOrㅋDefault(input, 0)+1);
+        }
 
 
         // 정렬하고
