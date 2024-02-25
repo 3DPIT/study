b0a375b097984f92b6295f3bbde4f71252143a54
Black <oksos0817@naver.com>
2024-02-25 15:51:02 +0900

    feat : 문제 풀이 셋업

---
+++ 02.studyData/10.Algorithm/2023/java/algorithm/src/main/java/com/example/algorithm/알고리즘/정렬/_1931_회의실배정.java
@@ -0,0 +1,31 @@
+package com.example.algorithm.알고리즘.정렬;
+
+/*
+https://www.acmicpc.net/problem/1931
+ */
+public class _1931_회의실배정 {
+    public static void main(String[] args) {
+
+    }
+}
+
+
+/*
+11
+1 4
+3 5
+0 6
+5 7
+3 8
+5 9
+6 10
+8 11
+8 12
+2 13
+12 14
+
+4
+
+hint
+(1,4), (5,7), (8,11), (12,14) 를 이용할 수 있다.
+ */
---
+++ 02.studyData/10.Algorithm/2023/java/algorithm/src/main/java/com/example/algorithm/알고리즘/정렬/_2910_빈도정렬.java
@@ -0,0 +1,29 @@
+package com.example.algorithm.알고리즘.정렬;
+
+
+/*
+https://www.acmicpc.net/problem/2910
+ */
+public class _2910_빈도정렬 {
+
+    public static void main(String[] args) {
+
+
+        // 정렬하고
+        // 정렬된것 숫자의 빈도가 같으면 먼저 나온것을 먼저로 한다.
+    }
+}
+
+/*
+5 2
+2 1 2 1 2
+
+2 2 2 1 1
+ */
+
+/*
+9 3
+1 3 3 3 2 2 2 1 1
+
+1 1 1 3 3 3 2 2 2
+ */
