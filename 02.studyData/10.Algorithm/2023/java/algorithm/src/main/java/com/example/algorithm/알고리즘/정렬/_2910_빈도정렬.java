e1ffb05c4b14d123ff287e91c03b46979f7364bb
박경민 <kyeongmin.park@mirero.co.kr>
2024-02-27 08:06:16 +0900

    feat : 빈도 정렬 알고리즘 완료 업로드

--- 02.studyData/10.Algorithm/2023/java/algorithm/src/main/java/com/example/algorithm/알고리즘/정렬/_2910_빈도정렬.java
+++ 02.studyData/10.Algorithm/2023/java/algorithm/src/main/java/com/example/algorithm/알고리즘/정렬/_2910_빈도정렬.java
@@ -18,54 +18,62 @@ public class _2910_빈도정렬 {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int numberCount = Integer.parseInt(st.nextToken());
-
-        Set<Integer> inputSet = new LinkedHashSet<>();
-
-        Map<Integer , Integer> m = new HashMap<>();
-        Map<Integer , Integer> m2 = new HashMap<>();
+//
+//        Set<Integer> inputSet = new LinkedHashSet<>();
+//
+//        Map<Integer , Integer> m = new HashMap<>();
+//        Map<Integer , Integer> m2 = new HashMap<>();
+//
+//        String[] inputs = br.readLine().split(" ");
+//        List<Integer> inputList = new ArrayList<>(inputs.length);
+//
+//        for(int i=0;i<N;i++){
+//            int input = Integer.parseInt(inputs[i]);
+//            inputSet.add(input);
+////            inputList.add(input);
+//            m.put(input, m.getOrDefault(input, 0)+1);
+//        }
+//
+//        Iterator<Integer> it = inputSet.iterator();
+//        for(int i=0;i<inputSet.size();i++){
+//            m2.put(it.next(),i);
+//        }
+//
+//        for(int i=0;i<N;i++){
+//            inputList.add(m.get(i));
+//        }
+//
+//        inputList.sort((o1,o2)->{
+//            if(m.get(o1).equals(m.get(o2))){
+//                return m2.get(m.get(o1)).compareTo(m2.get(m.get(o2)));
+//            }
+//
+//            return m.get(o1).compareTo(m.get(o2));
+//        });
 
         String[] inputs = br.readLine().split(" ");
-        List<Integer> inputList = new ArrayList<>(inputs.length);
-
-        for(int i=0;i<N;i++){
-            int input = Integer.parseInt(inputs[i]);
-            inputSet.add(input);
-//            inputList.add(input);
-            m.put(input, m.getOrDefault(input, 0)+1);
-        }
-
-        Iterator<Integer> it = inputSet.iterator();
-        for(int i=0;i<inputSet.size();i++){
-            m2.put(it.next(),i);
-        }
-
-        for(int i=0;i<N;i++){
-            inputList.add(m.get(i));
-        }
-
-        inputList.sort((o1,o2)->{
-            if(m.get(o1).equals(m.get(o2))){
-                return m2.get(m.get(o1)).compareTo(m2.get(m.get(o2)));
-            }
-
-            return m.get(o1).compareTo(m.get(o2));
-        });
-
         Map<Integer, Integer> map = new LinkedHashMap<>();
         for(int i=0;i<N;i++){
-            int message = sc.netInt();
-            map.put(message, map.getORDefault(message, 0)+1);
+            int message = Integer.parseInt(inputs[i]);
+            map.put(message, map.getOrDefault(message, 0)+1);
         }
 
-        Ingeger[] fre = map.keySet().toArray(new Integer[map.size()]);
+        Integer[] frequenceNumber = map.keySet().toArray(new Integer[map.size()]);
 
-        Arrays.sort(fre, new Comparator<Integer>(){
+        Arrays.sort(frequenceNumber, new Comparator<Integer>(){
             @Override
             public int compare(Integer o1, Integer o2){
                 return map.get(o2)-map.get(o1);
             }
-        })
-        })
+        });
+
+        for(int number: frequenceNumber){
+            for(int i=0;i<map.get(number);i++) {
+                bw.write(number + " ");
+            }
+        }
+
+        bw.flush();
 
 
         //정렬된 좌표를 ㅈ
