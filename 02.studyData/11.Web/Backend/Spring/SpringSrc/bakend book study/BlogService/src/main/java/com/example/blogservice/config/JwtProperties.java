b379cde4eccea9c71c8d1b3d204780ac305f5a8e
Black <oksos0817@naver.com>
2024-02-29 00:44:45 +0900

    feat : jwt 관련 설정 업로드

---
+++ 02.studyData/11.Web/Backend/Spring/SpringSrc/bakend book study/BlogService/src/main/java/com/example/blogservice/config/JwtProperties.java
@@ -0,0 +1,14 @@
+package com.example.blogservice.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;å
+@Setter
+@Getter
+@Component
+@ConfigurationProperties("jwt") //자바 클래스에 프로퍼티 값을 가져와서 사용하는 어노테이션
+public class JwtProperties{
+    private String issuer;
+    private String secretKey;
+}
---
+++ 02.studyData/11.Web/Backend/Spring/SpringSrc/bakend book study/BlogService/src/main/java/com/example/blogservice/config/jwt/TokenProvider.java
@@ -0,0 +1,83 @@
+package com.example.blogservice.config.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Header;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.RequiredArgsConstructor;
+import me.shinsunyoung.springbootdeveloper.config.JwtProperties;
+import me.shinsunyoung.springbootdeveloper.domain.User;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Date;
+import java.util.Set;
+
+@RequiredArgsConstructor
+@Service
+public class TokenProvider {
+    private  final JwtProperties jwtProperties;
+
+    public String generateToken(User user, Duration expiredAt){
+        Date now = new Date();
+        return makeToken(new Date(now.getTime()+ expiredAt.toMillis()), user);
+    }
+
+    // 1 JWT 토큰 생성 메서드
+    private String makeToken(Date expiry, User user){
+        Date now = new Date();
+
+        return Jwts.builder()
+                .setHeaderParam(Header.TYPE, Header.JWT_TYPE)// 헤더 typ: JWT
+                // 내용 iss : 3dpit@naver.com(properties 파일에서 설정한 값)
+                .setIssuer(jwtProperties.getIssuer())
+                //내용 iat : 현재 시간
+                .setIssuedAt(now)
+                //내용 exp : expiry 멤버 변숫값
+                .setExpiration(expiry)
+                //내용 sub : 유저의 이메일
+                .setSubject(user.getEmail())
+                // 클레임 id : 유저 ID
+                .claim("id",user.getId())
+                //서명 : 비밀과 함께 해시값을 HS256 방식으로 암호화
+                .signWith(SignatureAlgorithm.HS256, jwtProperties.getSecretKey())
+                .compact();
+    }
+
+    //2 JWT 토큰 유효성 검증 메서드
+    public boolean validToken(String token){
+        try{
+            Jwts.parser()
+                    .setSigningKey(jwtProperties.getSecretKey())//비밀값으로 복호화
+                    .parseClaimsJws(token);
+            return true;
+        }catch(Exception e){
+            return false;
+        }
+    }
+
+    //3 토큰 기반으로 인증 정보를 가져오는 메서드
+    public Authentication getAuthentication(String token){
+        Claims claims = getClaims(token);
+        Set<SimpleGrantedAuthority> authorities = Collections.singleton(new SimpleGrantedAuthority("ROLE_USET"));
+
+        return new UsernamePasswordAuthenticationToken(new org.springframework.security.core.userdetails.User(claims.getSubject(),"",authorities), token, authorities);
+    }
+
+    //4 토큰 기반으로 유저 ID를 가져오는 메서드
+    public Long getUserId(String token){
+        Claims claims = getClaims(token);
+        return claims.get("id",Long.class);
+    }
+
+    private Claims getClaims(String token){
+        return Jwts.parser()//클레임 조회
+                .setSigningKey(jwtProperties.getSecretKey())
+                .parseClaimsJws(token)
+                .getBody();
+    }
+}
