00f8894fbcd71ddc087afef9e7d2d37d3353d9d9
Black <oksos0817@naver.com>
2024-03-02 21:51:52 +0900

    feat : init 초기 설계 업로드

--- 02.studyData/10.Algorithm/2023/java/algorithm/src/main/java/com/example/algorithm/알고리즘/정렬/_1931_회의실배정.java
+++ 02.studyData/10.Algorithm/2023/java/algorithm/src/main/java/com/example/algorithm/알고리즘/정렬/_1931_회의실배정.java
@@ -1,18 +1,27 @@
 package com.example.algorithm.알고리즘.정렬;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
+import java.util.Map;
+import java.util.TreeMap;
 
 /*
 √ */
 public class _1931_회의실배정 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        
+        int N = Integer.parseInt(br.readLine());
+
+        Map<Integer,Integer> map = new TreeMap<>();
+        for(int i=0;i<N;i++){
+            String[] input = br.readLine().split(" ");
+            map.put(Integer.parseInt(input[0]),Integer.parseInt(input[1]));
+        }
+
+        for(int key : map.keySet()){
+            bw.write(key+" "+map.get(key)+"\n");
+        }
         // 시작시간 기준으로 오름차순, 끝시간 기준으로 오름차순
         // 0번 부터 n까지 검사 시작
         // 선택된것의 종료시간 이후에서 가장 처음꺼를 기준으로 계산 하여 count 계산
