730dfc545e633b81fe8303e1dc32e99daa5935b4
박경민 <kyeongmin.park@mirero.co.kr>
2024-03-06 07:47:37 +0900

    refactor : 누적합 적용 알고리즘 추가 시간초과 개선

--- 02.studyData/10.Algorithm/2023/java/algorithm/src/main/java/com/example/algorithm/알고리즘/누적합/_11659_구간합구하기4.java
+++ 02.studyData/10.Algorithm/2023/java/algorithm/src/main/java/com/example/algorithm/알고리즘/누적합/_11659_구간합구하기4.java
@@ -13,11 +13,13 @@ public class _11659_구간합구하기4 {
         int M = Integer.parseInt(inputs[1]);
         String[] InputNumbers = br.readLine().split(" ");
 
-        int[] sumNumberArrays = new int[N];
-        for(int i=0 ;i<N;i++){
-            sumNumberArrays[i] += Integer.parseInt(InputNumbers[i]);
+        int[] sumNumberArrays = new int[N+1];
+        sumNumberArrays[0]= Integer.parseInt(InputNumbers[0]);
+        for(int i=1 ;i<N;i++){
+            sumNumberArrays[i] =sumNumberArrays[i-1]+ Integer.parseInt(InputNumbers[i]);
         }
         /*
+        0 0+1 1+2 2+3
         5 4 3  2  1
         5 9 12 14 15
         1 2 3  4  5
@@ -37,10 +39,19 @@ public class _11659_구간합구하기4 {
 
         for(int i=0;i<M;i++) {
             String[] inputIndexs = br.readLine().split(" ");
-            int startIndex = Integer.parseInt(inputIndexs[0]);
-            int endIndex = Integer.parseInt(inputIndexs[1]);
-            int sum = sumNumberArrays[endIndex]-sumNumberArrays[startIndex-1];
+            int startIndex = Integer.parseInt(inputIndexs[0])-1;
+            int endIndex = Integer.parseInt(inputIndexs[1])-1;
+            int sum =0;
+            if(startIndex-1<0){
+                sum = sumNumberArrays[endIndex];
+            }
+            else{
+                sum = sumNumberArrays[endIndex]-sumNumberArrays[startIndex-1];
+            }
+/*
+            5 9 12 14 15
 
+  */
             bw.write(sum+"\n");
             bw.flush();
         }
