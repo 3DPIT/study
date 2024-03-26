0a8262e3c353827a6f05fc8e678731a6d1d6a568
Black <oksos0817@naver.com>
2024-03-26 23:47:58 +0900

    feat : mustache구현해서 Crud 실습

--- 02.studyData/00.Spring/Spring기초연습/awsSpring/src/main/java/com/dpit3/black/springboot/service/posts/PostsService.java
+++ 02.studyData/00.Spring/Spring기초연습/awsSpring/src/main/java/com/dpit3/black/springboot/service/posts/PostsService.java
@@ -3,13 +3,16 @@ package com.dpit3.black.springboot.service.posts;
 
 import com.dpit3.black.springboot.domain.posts.Posts;
 import com.dpit3.black.springboot.domain.posts.PostsRepository;
+import com.dpit3.black.springboot.web.dto.PostsListResponseDto;
 import com.dpit3.black.springboot.web.dto.PostsResponseDto;
 import com.dpit3.black.springboot.web.dto.PostsSaveRequestDto;
 import com.dpit3.black.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -35,5 +38,20 @@ public class PostsService {
         Posts entity = postsRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id="+ id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id){
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id="+ id));
+
+        postsRepository.delete(posts);
     }
 }
